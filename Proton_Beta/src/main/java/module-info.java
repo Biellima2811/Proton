d81146e5fbcbbac23fd394aec1667a6def171762
Gabriel Levi Lima Rodrigues <115875368+Biellima2811@users.noreply.github.com>
2026-05-21 21:39:26 -0300

    Delete Proton_Beta directory

--- Proton_Beta/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.proton {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.mycompany.proton to javafx.fxml;
-    exports com.mycompany.proton;
-}
